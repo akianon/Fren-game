5b163a68ffc06f2d675f1ce0fabf28137d5bc61c
denneisk <denneisk@protonmail.com>
2020-02-18 22:00:27 -0500

    Adding classfiles

---
+++ res/ClientPlayer.js
@@ -0,0 +1,12 @@
+var Player = require('Player.js');
+
+
+class ClientPlayer extends Player{
+	
+	constructor(x,y,id,sprite){
+	
+		super(x,y,id,sprite);
+	
+	}//constructor(x,y,id,sprite){
+	
+}//class ClientPlayer{
---
+++ res/ServerPlayer.js
@@ -0,0 +1,11 @@
+var Player = require('Player.js');
+
+class ServerPlayer extends Player{
+	
+	constructor(x,y,id,sprite){
+		
+		super(x,y,id,sprite);
+		
+	}//constructor(x,y,id,sprite){
+	
+}//class ServerPlayer{
